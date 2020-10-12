e2d5d4a909fdca97a3bbb7755cbc9db6d46bec02
Jack Fowler <OnigiriJack@users.noreply.github.com>
2020-10-12 16:01:14 +0900

    Tests  and implementation for geting a list of pokemon of a certain length

--- src/server.js
+++ src/server.js
@@ -1,9 +1,18 @@
 const pokeData = require("./data");
+const express = require("express");
 
 const setupServer = () => {
-  /**
-   * Create, set up and return your express server, split things into separate files if it becomes too long!
-   */
+  const app = express();
+  app.use(express.json());
+
+  app.get("/api/pokemon", (req, res) => {
+    //console.log(pokeData.pokemon);
+
+    if (req.query.n) res.send(pokeData.pokemon.slice(0, 15));
+    else res.send(pokeData.pokemon);
+  });
+
+  return app;
 };
 
 module.exports = { setupServer };
--- test/_pokemonApi.js
+++ test/_pokemonApi.js
@@ -2,7 +2,7 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 chai.use(chaiHttp);
 const { setupServer } = require("../src/server");
-
+chai.should();
 /*
  * This sprint you will have to create all tests yourself, TDD style.
  * For this you will want to get familiar with chai-http https://www.chaijs.com/plugins/chai-http/
@@ -14,4 +14,52 @@ describe("Pokemon API Server", () => {
   beforeEach(() => {
     request = chai.request(server);
   });
+
+  describe("pokemon", () => {
+    describe("GET", () => {
+      it("should return a list of all pokemon", async () => {
+        const { body } = await request.get("/api/pokemon");
+        body.length.should.equal(151);
+      });
+      it("when given a query parameter, it should return a list of n pokemon", async () => {
+        const { body } = await request.get("/api/pokemon").query({ n: 15 });
+        body.length.should.equal(15);
+      });
+    });
+    xdescribe("POST", () => {
+      it("should return status 418", async () => {
+        const res = await request.get("/teapot");
+        res.should.have.status(418);
+      });
+    });
+    xdescribe("PATCH", () => {
+      it("should return status 418", async () => {
+        const res = await request.get("/teapot");
+        res.should.have.status(418);
+      });
+    });
+    xdescribe("DELETE", () => {
+      it("should return status 418", async () => {
+        const res = await request.get("/teapot");
+        res.should.have.status(418);
+      });
+    });
+  });
+  /*
+    describe("attacks", () => {
+      it("should return the text/html 'world'", async () => {
+        const res = await request.get("/hello");
+        res.should.be.html;
+        res.text.should.equal("world");
+      });
+    });
+
+    describe("types", () => {
+      it("should return the text/html 'world'", async () => {
+        const res = await request.get("/hello");
+        res.should.be.html;
+        res.text.should.equal("world");
+      });
+    });
+*/
 });
